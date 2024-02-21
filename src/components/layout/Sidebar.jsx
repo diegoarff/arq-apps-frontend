e96257d889c0026d348bd54258400f9badbd01d2
Francisco J. Mendoza <103617075+fmbalza@users.noreply.github.com>
2024-02-21 12:51:16 -0400

    Update Sidebar
    New animations added
    
    color change
    
    Font change
    
    grid component and divider added

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { Box, Sheet, Typography } from '@mui/joy';
+import { Box, Grid, Sheet, Typography, Divider } from '@mui/joy'; // Se agrego el componente Grid
 import { useSubject } from '../../hooks/queries/useSubject';
 import { NavLink } from 'react-router-dom';
 
@@ -15,55 +15,62 @@ const Sidebar = () => {
 	}, {});
 
 	return (
-		<Box
-			style={{
-				height: '100%',
-				overflowY: 'scroll',
-			}}
-		>
+		<Box sx={{ backgroundColor: '#121316', p: 1.5 }}>
 			{status === 'pending' && <p>Loading...</p>}
 			{status === 'error' && <p>Error: {error.message}</p>}
 			{status === 'success' && (
-				<>
+				<Grid container direction="column" spacing={0} margin={0}>
 					{Object.entries(groupedSubjects)
-						.sort(([termA], [termB]) => termA.localeCompare(termB)) // Sort terms alphabetically
+						.sort(([termA], [termB]) => termA.localeCompare(termB))
 						.map(([term, subjects]) => (
-							<div key={term}>
-								<h3>Term {term}</h3>
-								<Sheet
-									variant="soft"
-									sx={{
-										p: 1,
-										display: 'flex',
-										flexDirection: 'column',
-										gap: 1,
-									}}
-								>
+							<Grid item key={term} margin={0.5}>
+								<Typography variant="h6" sx={{ fontWeight: 600, mb: 1 }}>
+									Term {term}
+								</Typography>
+
+								<Grid container direction="column" spacing={1}>
 									{subjects
-										.sort((a, b) => a.name.localeCompare(b.name)) // Sort subjects alphabetically
+										.sort((a, b) => a.name.localeCompare(b.name))
 										.map((subject) => (
-											<NavLink
-												key={subject._id}
-												to={`/subject/${subject._id}`}
-												style={({ isActive }) => {
-													return {
+											<Grid item key={subject._id}>
+												<NavLink
+													to={`/subject/${subject._id}`}
+													style={({ isActive }) => ({
 														textDecoration: 'none',
 														fontWeight: isActive ? 'bold' : '',
-													};
-												}}
-											>
-												<Sheet variant="outlined" sx={{ px: 2, py: 1 }}>
-													<Typography>{subject.name}</Typography>
-												</Sheet>
-											</NavLink>
+														color: isActive ? 'primary.main' : '', // Use Material UI's theme colors
+													})}
+												>
+													<Sheet
+														variant="outlined"
+														sx={{
+															px: 2,
+															py: 1,
+															borderRadius: 5,
+															cursor: 'pointer',
+															transition: 'all 0.2s ease-in-out',
+															backgroundColor: '#121316',
+															color: '#333',
+															'&:hover': {
+																backgroundColor: '#404146',
+																boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)',
+															},
+														}} // Hover effect
+													>
+														<Typography variant="body1">
+															{subject.name}
+														</Typography>
+													</Sheet>
+												</NavLink>
+											</Grid>
 										))}
-								</Sheet>
-							</div>
+									<Divider />
+								</Grid>
+							</Grid>
 						))}
-				</>
+				</Grid>
 			)}
 		</Box>
 	);
 };
-
 export default Sidebar;
