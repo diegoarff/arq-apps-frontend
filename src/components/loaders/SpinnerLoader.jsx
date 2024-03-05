7a714ae1efa33745a5b17168f108e6337cb2cf91
Francisco J. Mendoza <103617075+fmbalza@users.noreply.github.com>
2024-03-05 17:46:19 -0400

    SpinnerLoader

---
+++ src/components/loaders/SpinnerLoader.jsx
@@ -0,0 +1,7 @@
+import CircularProgress from '@mui/joy/CircularProgress';
+
+const SpinnerLoader = () => {
+	return <CircularProgress color="primary" variant="plain" size="lg" />;
+};
+
+export default SpinnerLoader;
