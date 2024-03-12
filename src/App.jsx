0961644dbba7d2fe4030980598baee9e79e1d4e8
diegoarff <diegorinconf1@gmail.com>
2024-03-12 15:52:07 -0400

    add VITE_LOCALE variable and update theme based on locale

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import Snack from './components/Snack';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes/router';
 import themes from './themes';
-import { useAuthStore } from './store/useAuthStore';
 
 const App = () => {
-	const user = useAuthStore((state) => state.user);
-	const localeTheme = user ? themes[user.university.locale] : themes['default'];
+	const localeTheme = themes[import.meta.env.VITE_LOCALE] ?? themes['default'];
 	return (
 		<CssVarsProvider defaultMode="dark" theme={localeTheme}>
 			<Snack />
