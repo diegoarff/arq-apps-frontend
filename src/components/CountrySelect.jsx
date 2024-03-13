1224ddaa219b872465886932c6f0f9e79cb5a9d7
diegoarff <diegorinconf1@gmail.com>
2024-03-13 15:05:50 -0400

    feat: add a select for countries
    Co-authored-by: César Cayaffa <cesarcayaffa0@gmail.com>
    Co-authored-by: Juan Romero <juanjoseromerozabala47@gmail.com>
    Co-authored-by: Francisco Mendoza <fmbalza@gmail.com>

---
+++ src/components/CountrySelect.jsx
@@ -0,0 +1,41 @@
+import { Select, Option } from '@mui/joy';
+
+const CountrySelect = () => {
+	const currentUrl = window.location.href.split('/')[2];
+
+	const options = [
+		{
+			label: 'Venezuela',
+			value: 'https://arq-apps-frontend-es.vercel.app/',
+			country: 've',
+		},
+		{
+			label: 'Estados Unidos',
+			value: 'https://arq-apps-frontend-en.vercel.app/',
+			country: 'us',
+		},
+		{
+			label: 'Francia',
+			value: 'https://arq-apps-frontend-fr.vercel.app/',
+			country: 'fr',
+		},
+	];
+
+	return (
+		<Select label="País" defaultValue={currentUrl}>
+			{options.map((option, index) => (
+				<Option
+					key={index}
+					value={option.value}
+					onClick={() => {
+						window.location.href = option.value;
+					}}
+				>
+					{option.label}
+				</Option>
+			))}
+		</Select>
+	);
+};
+
+export default CountrySelect;
